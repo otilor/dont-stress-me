51d40e37e5e5b3ca8067ddd5962883d5f036eb21
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-08-24 16:44:28 +0100

    Refactor: split up response details to a struct

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/GabrielFemi/dont-stress-me
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,37 +2,68 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
 	"github.com/google/go-github/github"
 	"github.com/sirupsen/logrus"
 )
 
+var ctx = context.Background()
+
+
 func main() {
 	r := gin.Default()
+
+	type Details struct {
+		Username string
+		Public_repositories int
+	}
+
 	r.GET("/repositories/:username", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message":"pong",
-			"username":c.Param("username"),
-			"public_repositories": len (getRepositoriesFromUsername(c.Param("username"))),
-		})
+		c.JSON(200, &Details{Username: c.Param("username"), Public_repositories: len(getRepositoriesFromUsername(c.Param("username")))})
 	})
 	r.Run("127.0.0.1:7000")
 }
 
-
-
-
-func getRepositoriesFromUsername(username string) []*github.Repository{
-
-	client := github.NewClient(nil)
-	ctx := context.Background()
-
-	// get the list of organizations for a specific user.
-
-	orgs, _, err := client.Repositories.List(ctx, username, nil)
+func ExampleClient() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "127.0.0.1:6379",
+		Password: "",
+		DB: 0,
+	})
+	err := rdb.Set("key", "value", 10000)
 	if err != nil {
 		logrus.Fatalln(err)
 	}
 
-	return orgs
+	fmt.Println("Successfully set key!")
+}
+
+func cacheDetails(details string) {
+
+}
+
+func getRepositoriesFromUsername(username string) []*github.Repository{
+
+	client := github.NewClient(nil)
+
+
+	// get the list of organizations for a specific user.
+
+	opt := &github.RepositoryListOptions{Type: "owner", Sort: "updated", Direction: "desc"}
+	var allRepos []*github.Repository
+
+	for {
+		repos, resp, err := client.Repositories.List(ctx, username, opt)
+		if err != nil {
+			logrus.Fatalln(err)
+		}
+		allRepos = append(allRepos, repos...)
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+	return allRepos
 }
